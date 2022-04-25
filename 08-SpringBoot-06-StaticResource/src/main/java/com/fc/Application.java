e54b9080da0b9b8ecded9e3eb97992b5c72eb9f6
zch <2631994547@qq.com>
2022-04-25 20:12:57 +0800

    欢迎页面 favicon

---
+++ 08-SpringBoot-06-StaticResource/src/main/java/com/fc/Application.java
@@ -0,0 +1,21 @@
+package com.fc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    // 1./META-INF/resources
+
+    // 2./resources
+
+    // 3.static
+
+    // 4.public
+
+}
